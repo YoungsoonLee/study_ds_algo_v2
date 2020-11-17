6915c7be56cdf598cec27e81d2d33c1a7933a80c
Ted Youngsoon Lee <youngtip@gmail.com>
2020-11-16 23:39:38 -0800

    .

--- linkedlist/singly-linkedlist/main.go
+++ linkedlist/singly-linkedlist/main.go
@@ -149,3 +149,187 @@ func (ll *LinkedList) Delete(position int) (interface{}, error) {
 	ll.size--
 	return current.data, nil
 }
+
+func kthFromEnd(head *ListNode, n int) *ListNode {
+	first, second := head, head
+	for ; n > 0; n-- {
+		second = second.next
+	}
+	for ; second.next != nil; first, second = first.next, second.next {
+	}
+	first.next = first.next.next
+	return first
+}
+
+func hasCycle(head *ListNode) bool {
+	fast, slow := head, head
+	for fast != nil && fast.next != nil {
+		fast = fast.next.next
+		slow = slow.next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
+
+func findLoopBeginning(head *ListNode) *ListNode {
+	fast, slow := head, head
+	loopExists := false
+	for fast != nil && fast.next != nil {
+		fast = fast.next.next
+		slow = slow.next
+		if fast == slow {
+			loopExists = true
+			break
+		}
+	}
+
+	if loopExists {
+		slow = head
+		for slow != fast {
+			fast = fast.next
+			slow = slow.next
+		}
+		return slow
+	}
+	return nil
+}
+
+func findLoopBeginning2(head *ListNode) int {
+	fast, slow := head, head
+	loopExists := false
+	for fast != nil && fast.next != nil {
+		fast = fast.next.next
+		slow = slow.next
+		if fast == slow {
+			loopExists = true
+			break
+		}
+	}
+	if loopExists {
+		counter := 1
+		fast = fast.next
+		for slow != fast {
+			fast = fast.next
+			counter++
+		}
+		return counter
+	}
+	return 0
+}
+
+func (ll *LinkedList) sortedInsert(data int) {
+	newNode := &ListNode{
+		data: data,
+	}
+	if ll.head == nil || ll.head.data.(int) >= data {
+		newNode.next = ll.head
+		ll.head = newNode
+		return
+	}
+	current := ll.head
+	for current.next != nil && current.next.data.(int) < data {
+		current = current.next
+	}
+	newNode.next = current.next
+	current.next = newNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev, current *ListNode
+
+	for current = head; current != nil; current = current.next {
+		current.next, prev, current = prev, current, current.next
+	}
+	return prev
+}
+
+func reverseList2(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	h := reverse(head)
+	head.next = nil
+	return h
+}
+
+func reverse(current *ListNode) *ListNode {
+	if current == nil {
+		return nil
+	}
+	temp := reverse(current.next)
+	if temp == nil {
+		return current
+	}
+	current.next.next = current
+	return temp
+}
+
+func getIntersectionNode(head1, head2 *ListNode) *ListNode {
+	for head1 != nil {
+		temp := head2
+		for temp != nil {
+			if temp == head1 {
+				return head1
+			}
+			temp = temp.next
+		}
+		head1 = head1.next
+	}
+	return nil
+}
+
+func getIntersectionNode2(head1, head2 *ListNode) *ListNode {
+	len1, len2 := findLen(head1), findLen(head2)
+	if len1 > len2 {
+		for ; len1 > len2; len1-- {
+			head1 = head1.next
+		}
+	} else {
+		for ; len2 > len1; len2-- {
+			head2 = head2.next
+		}
+	}
+	for head1 != head2 {
+		head1, head2 = head1.next, head2.next
+	}
+	return head1
+}
+
+func findLen(head *ListNode) int {
+	i := 0
+	for ; head != nil; head = head.next {
+		i++
+	}
+	return i
+}
+
+func middleNode(head *ListNode) *ListNode {
+	i := findLen(head)
+	count, target := 0, (i/2)+1
+	for {
+		count++
+		if count == target {
+			return head
+		}
+		head = head.next
+	}
+}
+
+func middleNode2(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil && fast.next != nil {
+		fast = fast.next.next
+		slow = slow.next
+	}
+	return slow
+}
+
+func printListInReverse(head *ListNode) {
+	if head == nil {
+		return
+	}
+	printListInReverse(head.next)
+	fmt.Print(head.data)
+}
