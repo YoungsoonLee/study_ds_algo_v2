3ebe3d2b4bce3a38b6addeed772c4c6b5e4344de
Ted Youngsoon Lee <youngtip@gmail.com>
2020-12-08 23:57:39 -0800

    .

--- tree/binary-tree/main.go
+++ tree/binary-tree/main.go
@@ -66,7 +66,92 @@ func insert(root *BinaryTreeNode, v int) *BinaryTreeNode {
 }
 
 func InOrder(root *BinaryTreeNode) {
+	if root == nil {
+		return
+	}
 
+	InOrder(root.left)
+	fmt.Printf("%d", root.data)
+	InOrder(root.right)
+}
+
+func InOrderWalk(root *BinaryTreeNode, ch chan int) {
+	if root == nil {
+		return
+	}
+
+	InOrderWalk(root.left, ch)
+	ch <- root.data
+	InOrderWalk(root.right, ch)
+}
+
+func InOrderWalker(root *BinaryTreeNode) <-chan int {
+	ch := make(chan int)
+	go func() {
+		InOrderWalk(root, ch)
+		close(ch)
+	}()
+	return ch
+}
+
+func PostOrder(root *BinaryTreeNode) {
+	if root == nil {
+		return
+	}
+
+	PostOrder(root.left)
+	PostOrder(root.right)
+	fmt.Printf("%d", root.data)
+}
+
+func PostOrderWalk(root *BinaryTreeNode, ch chan int) {
+	if root == nil {
+		return
+	}
+
+	PostOrderWalk(root.left, ch)
+	PostOrderWalk(root.right, ch)
+	ch <- root.data
+}
+
+func PostOrderWalker(root *BinaryTreeNode) <-chan int {
+	ch := make(chan int)
+	go func() {
+		PostOrderWalk(root, ch)
+		close(ch)
+	}()
+	return ch
+}
+
+// BFS
+// level order
+func LevelOrder(root *BinaryTreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+
+	var result [][]int
+	queue := []*BinaryTreeNode{root}
+
+	for len(queue) > 0 {
+		qlen := len(queue)
+		var level []int
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			level = append(level, node.data)
+			queue = queue[1:]
+
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+		}
+		result = append(result, level)
+	}
+	return result
 }
 
 func main() {
@@ -83,4 +168,21 @@ func main() {
 		}
 		fmt.Printf("%d", v)
 	}
+
+	fmt.Println()
+
+	InOrder(t1)
+	fmt.Println()
+
+	c = InOrderWalker(t1)
+	for {
+		v, ok := <-c
+		if !ok {
+			break
+		}
+		fmt.Printf("%d", v)
+	}
+
+	fmt.Println()
+	fmt.Println(LevelOrder(t1))
 }
