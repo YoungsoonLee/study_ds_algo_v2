d5d6aede5806f296fb5258465f42c474f501a374
Ted Youngsoon Lee <youngtip@gmail.com>
2020-12-14 16:27:36 -0800

    .

--- tree/binary-tree/main.go
+++ tree/binary-tree/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"sort"
+	"strconv"
 )
 
 type BinaryTreeNode struct {
@@ -294,6 +296,7 @@ func Size2(root *BinaryTreeNode) int {
 	return result
 }
 
+/*
 func LevelOrderBottomUp(root *BinaryTreeNode) [][]int {
 	if root == nil {
 		return [][]int{}
@@ -325,6 +328,7 @@ func LevelOrderBottomUp(root *BinaryTreeNode) [][]int {
 	}
 	return result
 }
+*/
 
 func DeleteTree(root *BinaryTreeNode) *BinaryTreeNode {
 	if root == nil {
@@ -484,6 +488,338 @@ func CompareStructures(root1, root2 *BinaryTreeNode) bool {
 	return CompareStructures(root1.left, root2.left) && CompareStructures(root1.right, root2.right)
 }
 
+func DiameterOfBinaryTree(root *BinaryTreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	var diameter int
+	Diameter(root, &diameter)
+	return diameter
+}
+
+func Diameter(root *BinaryTreeNode, diameter *int) int {
+	if root == nil {
+		return 0
+	}
+
+	leftDepth := Diameter(root.left, diameter)
+	rightDepth := Diameter(root.right, diameter)
+
+	if leftDepth+rightDepth > *diameter {
+		*diameter = leftDepth + rightDepth
+	}
+
+	return max(leftDepth, rightDepth) + 1
+}
+
+func Diameter2(root *BinaryTreeNode) int {
+	diameter := 0
+
+	var depth func(node *BinaryTreeNode) int
+	depth = func(node *BinaryTreeNode) int {
+		if node == nil {
+			return 0
+		}
+
+		leftDepth := depth(node.left)
+		rightDepth := depth(node.right)
+		diameter = max(diameter, leftDepth+rightDepth)
+
+		return max(leftDepth, rightDepth) + 1
+	}
+	depth(root)
+	return diameter
+}
+
+func maxLevelSum(root *BinaryTreeNode) (elements []int, maxSum, level int) {
+	if root == nil {
+		return elements, maxSum, level
+	}
+
+	var result [][]int
+	levelNumber := 0
+	queue := []*BinaryTreeNode{root}
+
+	for len(queue) > 0 {
+		qlen := len(queue)
+		var currentLevel []int
+		sum := 0
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			currentLevel = append(currentLevel, node.data)
+			sum += node.data
+			queue = queue[1:]
+
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+		}
+		if sum > maxSum {
+			maxSum = sum
+			elements = currentLevel
+			level = levelNumber
+		}
+		result = append(result, currentLevel)
+		levelNumber++
+	}
+	return elements, maxSum, level
+}
+
+func BinaryTreePaths(root *BinaryTreeNode) []string {
+	result := make([]string, 0)
+	paths(root, "", &result)
+	return result
+}
+
+func paths(root *BinaryTreeNode, prefix string, result *[]string) {
+	if root == nil {
+		return
+	}
+
+	if len(prefix) == 0 {
+		prefix += strconv.Itoa(root.data)
+	} else {
+		prefix += "->" + strconv.Itoa(root.data)
+	}
+
+	// leaf
+	if root.left == nil && root.right == nil {
+		*result = append(*result, prefix+"\n")
+		return
+	}
+
+	paths(root.left, prefix, result)
+	paths(root.right, prefix, result)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func HasPathSum(root *BinaryTreeNode, sum int) bool {
+	allSums := make([]int, 0)
+	getAllSums(root, &allSums, 0)
+	for _, val := range allSums {
+		if sum == val {
+			allSums = []int{}
+			return true
+		}
+	}
+	allSums = []int{}
+	return false
+}
+
+func getAllSums(root *BinaryTreeNode, allSums *[]int, currSum int) {
+	if root != nil {
+		currSum += root.data
+		if root.left == nil && root.right == nil {
+			*allSums = append(*allSums, currSum)
+		} else {
+			getAllSums(root.left, allSums, currSum)
+			getAllSums(root.right, allSums, currSum)
+		}
+	}
+}
+
+//
+func Sum(root *BinaryTreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return (root.data + Sum(root.left) + Sum(root.right))
+}
+
+func Sum2(root *BinaryTreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	var result int
+	queue := []*BinaryTreeNode{root}
+	for len(queue) > 0 {
+		qlen := len(queue)
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			result += node.data
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+		}
+	}
+	return result
+}
+
+func InvertTree(root *BinaryTreeNode) *BinaryTreeNode {
+	if root != nil {
+		root.left, root.right = InvertTree(root.right), InvertTree(root.left)
+	}
+	return root
+}
+
+func InvertTree2(root *BinaryTreeNode) {
+	if root == nil {
+		return
+	}
+	root.left, root.right = root.right, root.left
+	InvertTree2(root.left)
+	InvertTree2(root.right)
+	return
+}
+
+func checkMirror(root1, root2 *BinaryTreeNode) bool {
+	if root1 == nil && root2 == nil {
+		return true
+	}
+	if root1 == nil || root2 == nil {
+		return false
+	}
+	if root1.data != root2.data {
+		return false
+	}
+
+	return checkMirror(root1.left, root2.right) && checkMirror(root1.right, root2.left)
+}
+
+func LCA(root *BinaryTreeNode, a, b int) *BinaryTreeNode {
+	if root == nil {
+		return root
+	}
+
+	if root.data == a || root.data == b {
+		return root
+	}
+
+	left := LCA(root.left, a, b)
+	right := LCA(root.right, a, b)
+
+	if left != nil && right != nil {
+		return root
+	}
+
+	if left != nil {
+		return left
+	} else {
+		return right
+	}
+}
+
+func BuildBinaryTree(preOrder []int, inOrder []int) *BinaryTreeNode {
+	if len(preOrder) == 0 || len(inOrder) == 0 {
+		return nil
+	}
+	inOrderIndex := findArray(inOrder, preOrder[0])
+
+	left := BuildBinaryTree(preOrder[1:inOrderIndex+1], inOrder[:inOrderIndex])
+	right := BuildBinaryTree(preOrder[inOrderIndex+1:], inOrder[inOrderIndex+1:])
+
+	return &BinaryTreeNode{
+		data:  preOrder[0],
+		left:  left,
+		right: right,
+	}
+
+}
+
+func findArray(A []int, target int) int {
+	for i, x := range A {
+		if x == target {
+			return i
+		}
+	}
+	return -1
+}
+
+func ZigzagLevelOrder(root *BinaryTreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+
+	queue := []*BinaryTreeNode{root}
+	var res [][]int
+	leftToright := false
+	for {
+		qlen := len(queue)
+		if qlen == 0 {
+			break
+		}
+
+		var level []int
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			level = append(level, node.data)
+			queue = queue[1:]
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+		}
+
+		if leftToright {
+			reverse(level)
+		}
+		res = append(res, level)
+		leftToright = !leftToright
+	}
+	return res
+}
+
+func reverse(list []int) {
+	for i := 0; i < len(list)/2; i++ {
+		list[i], list[len(list)-1] = list[len(list)-1], list[i]
+	}
+}
+
+func VerticalTraversal(root *BinaryTreeNode) [][]int {
+	data := [][]int{}
+	preorder(root, 0, 0, &data)
+	sort.Slice(data, func(i, j int) bool {
+		if data[i][0] == data[j][0] {
+			if data[i][1] == data[j][1] {
+				return data[i][2] < data[j][2]
+			}
+			return data[i][1] > data[j][1]
+		}
+		return data[i][0] < data[j][0]
+	})
+	lastX := data[0][0]
+	traversal := [][]int{{}}
+	for _, v := range data {
+		if v[0] != lastX {
+			traversal = append(traversal, []int{})
+			lastX = v[0]
+		}
+		traversal[len(traversal)-1] = append(traversal[len(traversal)-1], v[2])
+	}
+	return traversal
+}
+
+func preorder(node *BinaryTreeNode, x, y int, data *[][]int) {
+	if node == nil {
+		return
+	}
+	*data = append(*data, []int{x, y, node.data})
+	preorder(node.left, x-1, y-1, data)
+	preorder(node.right, x+1, y+1, data)
+}
+
+func ConnectSiblings(root *BinaryTreeNode) *BinaryTreeNode {
+
+}
+
 func main() {
 	t1 := NewBinaryTree(10, 1)
 
@@ -520,4 +856,6 @@ func main() {
 
 	fmt.Println(Size(t1))
 	fmt.Println(Size2(t1))
+
+	fmt.Println(BinaryTreePaths(t1))
 }
