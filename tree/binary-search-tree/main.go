4013742ad51f06bf99b3f3f595dd772c7b677d75
Ted Youngsoon Lee <youngtip@gmail.com>
2020-12-22 22:57:07 -0800

    .

--- tree/binary-search-tree/main.go
+++ tree/binary-search-tree/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type BSTNode struct {
 	data  int
 	left  *BSTNode
@@ -171,4 +173,163 @@ func IsBST(root *BSTNode) bool {
 		return false
 	}
 	return true
+}
+
+func IsBST2(root *BSTNode, prev *int) bool {
+	if root == nil {
+		return true
+	}
+
+	if !IsBST2(root.left, prev) {
+		return false
+	}
+
+	if root.data < *prev {
+		return false
+	}
+	*prev = root.data
+
+	return IsBST2(root.right, prev)
+}
+
+func BST2DLL(root *BSTNode) {
+	if root == nil || (root.left == nil && root.right == nil) {
+		return
+	}
+	BST2DLL(root.left)
+	BST2DLL(root.right)
+
+	currRight := root.right
+	root.right = root.left
+	root.left = nil
+	for root.right != nil {
+		root = root.right
+	}
+	root.right = currRight
+}
+
+func SortedArrayToBST(A []int) *BSTNode {
+	if A == nil {
+		return nil
+	}
+
+	return helper(A, 0, len(A)-1)
+}
+
+func helper(A []int, low int, high int) *BSTNode {
+	if low > high {
+		return nil
+	}
+
+	mid := low + (high-low)/2
+	node := new(BSTNode)
+	node.data = A[mid]
+	node.left = helper(A, low, mid-1)
+	node.right = helper(A, mid+1, high)
+	return node
+}
+
+func kthSmallest(root *BSTNode, k int) *BSTNode {
+	counter := 0
+	return helper2(root, k, &counter)
+}
+
+func helper2(root *BSTNode, k int, counter *int) *BSTNode {
+	if root == nil {
+		return nil
+	}
+
+	left := helper2(root.left, k, counter)
+	if left != nil {
+		return left
+	}
+
+	*counter++
+	if *counter == k {
+		return root
+	}
+
+	return helper2(root.right, k, counter)
+}
+
+func FloorInBST(root *BSTNode, key int) *BSTNode {
+	if root == nil {
+		return root
+	}
+
+	if key > root.data {
+		r := FloorInBST(root.right, key)
+		if r == nil {
+			return root
+		} else {
+			return r
+		}
+	} else if key < root.data {
+		return FloorInBST(root.left, key)
+	} else {
+		return root
+	}
+}
+
+func CeilInBST(root *BSTNode, key int) *BSTNode {
+	if root == nil {
+		return root
+	}
+
+	if root.data == key {
+		return root
+	} else if root.data < key {
+		return CeilInBST(root.right, key)
+	} else {
+		l := CeilInBST(root.left, key)
+		if l != nil {
+			return l
+		}
+	}
+	return root
+}
+
+func RangePrinter(root *BSTNode, K1, K2 int) {
+	if root == nil {
+		return
+	}
+
+	if root.data >= K1 {
+		RangePrinter(root.left, K1, K2)
+	}
+	if root.data >= K1 && root.data <= K2 {
+		fmt.Print(" ", root.data)
+	}
+	if root.data <= K2 {
+		RangePrinter(root.right, K1, K2)
+	}
+}
+
+func RangePrinter2(root *BSTNode, K1, K2 int) {
+	if root == nil {
+		return
+	}
+
+	var result [][]int
+	queue := []*BSTNode{root}
+
+	for len(queue) > 0 {
+		qlen := len(queue)
+		var level []int
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			level = append(level, node.data)
+			queue = queue[1:]
+			if node.data >= K1 && node.data <= K2 {
+				fmt.Print(" ", node.data)
+			}
+			if node.left != nil && node.data >= K1 {
+				queue = append(queue, node.left)
+			}
+			if node.right != nil && node.data <= K2 {
+				queue = append(queue, node.right)
+			}
+		}
+		result = append(result, level)
+	}
 }
